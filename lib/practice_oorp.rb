2d831fda5b84a876c73ab6b42420127ad132d6f7
Mario Lopez <ez0212sp@icloud.com>
2021-01-13 17:57:32 -0500

    commit tree fe0457f7b50035eed03fb23ac19393231ce59a5e

---
+++ lib/practice_oorp.rb
@@ -0,0 +1,33 @@
+
+# # class Person
+# #     def say_hello(name)
+# #     puts "#{name}"
+# #     end
+# # end
+
+
+# class Person
+#     def initialize(name)
+#     @name = name 
+#     end
+
+# def name
+#     puts @name
+# end
+# end
+
+# def read 
+#     puts "#{@name} can making the reading really well"
+# end
+# end
+
+
+
+pete = Person.new("Pete", "March 16th 2010","199 Learn Way")
+
+def call_name 
+    #main
+    puts self
+end
+
+pete.call_name
